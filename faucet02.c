fd50e2d171f0b78735eae9e38eb5f83dac2fa059
Ryu1633 <113009898+Ryu1633@users.noreply.github.com>
2023-02-16 22:42:41 +0900

    Add files via upload

---
+++ faucet02.c
@@ -0,0 +1,194 @@
+#define F_CPU   16000000L
+
+#include	<avr/io.h>
+#include	<avr/interrupt.h>
+#include	<stdio.h>
+#include	"lcd.h" //컴파일용
+
+#define OC0 PB4 //워터펌프 제어 PWM 신호 출력핀
+#define	OC2	PB7 //서보모터 제어 PWM 신호 출력핀
+
+#define SENSOR0	0   //PIR센서 채널
+#define SENSOR1 1   //왼쪽 적외선 센서 채널
+#define SENSOR2 2   //오른쪽 적외선 센서 체널
+
+volatile int timer0_rq = 0;                   //타이머0 인터럽트 요청 변수
+volatile int timer2 = 0;                      //타이머2 정지 판정 변수
+volatile int  adc_rq = 0;                       //ADC인터럽트 요청 변수
+volatile int cnt = 0;                         //대기모드 전환 변수
+volatile int adc_result[3];                   //ADC변환값을 저장하는 int형 배열
+volatile int channel = SENSOR0;               //ADC 채널 선택 변수 초기값 = PIR센서
+enum {IDLE, WATER, HOT, COLD} state = IDLE;   //상태 선언과 초기화
+
+
+//#################### 컴파일용 ########################################
+//대충 LCD에 쓰이는 함수
+void	LCD_str_write(unsigned int row, unsigned int col, char *str)
+{
+	int	i;
+
+	set_cursor(row, col);
+	for(i=0; (i+col < 0x40) && (str[i] != '\0'); i++)
+		LCD_data_write(str[i]);
+}
+//######################################################################
+
+//타이머/카운터0 인터럽트
+ISR(TIMER0_OVF_vect) {
+    timer0_rq = 0;  //타이머0 인터럽트 응답
+}
+
+//타이머/카운터0 인터럽트
+ISR(TIMER2_OVF_vect) {
+    timer2++;
+    //0.016 * 200초 후 타이머2 정지
+    if(timer2 >= 200) {
+        TCCR2 &= TCCR2 & 0b11111000;    //타이머2 정지
+        TIMSK &= TIMSK & 0b10111111;    //타이머2 인터럽트 비활성화
+    }
+}
+
+//ADC인터럽트
+ISR(ADC_vect) {
+    adc_result[channel] = ADC;  //현재 채널의 ADC값 저장
+	adc_rq = 0;                 //ADC인터럽트 응답 완료
+}
+
+//타이머/카운터0 초기화 함수
+void TIMER0_init(void) {
+	TCCR0 = 1<<WGM01 | 1<<WGM00;	        //타이머0 고속 PWM 모드
+	//TCCR0 |= 1<<CS02 | 1<<CS01 | 1<<CS00;	//1024분주, 0.016384, 61Hz
+	TCCR0 |= 1<<COM01;                  	//타이머0의 OCR과 TCNT가 일치하면 LOW, OVF발생시 HIGH
+	OCR0 = 130;                             //1초당 평균전압 4.5V ~ 4.8V가 되는 OCR값, 9V의 55.55% duty = 5V
+    //TIMSK |= 1<<TOIE0;                      //타이머0 인터럽트 활성화
+}
+
+//타이머/카운터2 초기화 함수
+void TIMER2_init(void) {
+    DDRB |= 1<<OC2;                 //PB7 출력핀 설정 = 서보모터 PWM 출력핀으로 설정
+    TCCR2 = 1<<WGM21 | 1<<WGM20;    //타이머2 고속 PWM 모드
+    TCCR2 |= 1<<COM01;              //타이머2의 OCR과 TCNT가 일치하면 LOW, OVF발생시 HIGH
+    OCR2 = (int)34;                 //서보모터 각도 초기값 = 냉수방향
+}
+
+//ADC초기화 함수
+void ADC_init(void) {
+    ADCSRA |= 1<<ADEN | 1<<ADIE | 7;    //ADC 활성화(1<<ADEN), ADC인터럽트 활성화(1<<ADIE), 128분주 = ADCSRA = 1000 1111
+	ADCSRA |= 1<<ADFR;                  //ADC 연속모드 설정 =   ADCSRA = 1010 1111
+}
+
+int main(void) {
+//############################## 컴파일용 ##############################
+    char lcd_string[4][0x40];           //LCD에 표시할 글자를 저장할 char 배열
+	LCD_init();                         //LCD초기화
+
+    sprintf(lcd_string[0], "ADC0=");    //PIR센서 측정값
+	sprintf(lcd_string[1], "ADC1=");    //왼쪽 적외선 센서 측정값
+	sprintf(lcd_string[2], "ADC2=");    //오른쪽 적외선 센서 측정값
+    sprintf(lcd_string[3], "CNT=");     //컴파일 결과 확인용
+
+	LCD_str_write(0, 0, lcd_string[0]); //PIR 센서
+    LCD_str_write(0, 9, lcd_string[3]); //CNT
+	LCD_str_write(1, 0, lcd_string[1]); //적외선 센서 좌
+	LCD_str_write(1, 8, lcd_string[2]); //적외선 센서 우
+//#####################################################################
+    
+	ADC_init();     //ADC초기화
+    TIMER0_init();  //타이머/카운터0 초기화
+    TIMER2_init();  //타이머/카운터2 초기화
+
+    sei();		    //전역 인터럽트 활성화(이 시점 부터 ADC인터럽트 발생 = ADC값을 받기 시작한다)
+
+//##################################### 메인 루프 시작 #####################################
+    while(1) {
+        switch(state) {
+            case IDLE:                                      //대기모드
+                //동작 인식하면 급수 시작
+                if(adc_result[0] > 500) {        
+                    TCCR0 |= 1<<CS02 | 1<<CS01 | 1<<CS00;	//타이머0 작동 시작, 1024분주, 주기 = 0.016384, 61Hz
+                    TIMSK |= 1<<TOIE0;                      //타이머0 인터럽트 활성화
+                    state = WATER;                          //급수모드로 변경
+                }
+            break;
+
+            case WATER:                 //급수모드
+                DDRB |= 1<<OC0;         //PB4 출력핀으로 설정 = 워터펌프 PWM 출력핀
+                
+//##################################### ADC채널 변경 #####################################
+                //ADC채널을 설정하는 변수값 변경
+                channel = channel == SENSOR0 ? SENSOR1 : (channel == SENSOR1 ? SENSOR2 : SENSOR0);
+                //ADC채널 변경을 위해 ADMUX레지스터 변경
+		        ADMUX = channel == SENSOR0 ? 0b00000000 : (channel == SENSOR1 ? 0b00000001 : 0b00000010);
+                timer0_rq = 1;  //타이머0 인터럽트 요청
+                while(timer0_rq);   //타이머/카운터0 인터럽트 발생까지 대기 = 16ms대기 = ADC 채널변경을 위한 시간지연
+//########################################################################################
+
+
+
+//##################################### 대기모드 전환 #####################################
+                //일정시간 동작이 없으면 대기모드로 전환 + 냉수로 변경
+                cnt = adc_result[0] < 500 ? cnt+1 : 0;
+                if(cnt >= 300) {
+                    state = IDLE;               //대기모드로 전환
+                    timer2 = 0;                 //타이머2 정지를 위한 변수값 초기화
+                    OCR2 = (int)34;             //OCR2 값 변경 = 냉수방향
+                    TCCR2 |= 1<<CS22 | 1<<CS20; //타이머2 작동
+                    TIMSK |= 1<<TOIE2;          //타이머2 인터럽트 활성화
+
+                    cnt = 0;                    //대기모드 전환 카운트 초기화
+                    channel = SENSOR0;          //PIR센서 채널로 변경
+                    ADMUX = 0b00000000;         //PIR센서 채널로 변경을 위해 ADMUX 레지스터 변경
+                    timer0_rq = 1;              //타이머0 인터럽트 요청
+                    while(timer0_rq);           //타이머/카운터0 인터럽트 발생까지 대기 = 16ms대기 = ADC 채널변경을 위한 시간지연
+                    DDRB &= DDRB & 0b10000000;  //타이머0 PWM 신호 출력 중지 = 워터펌프 정지
+                    TCCR0 &= TCCR0 & 0b11111000;    //타이머2 정지
+                    TIMSK &= TIMSK & 0b10111111;    //타이머2 인터럽트 비활성화
+                }//end if
+//########################################################################################
+
+                if(adc_result[1] <= 980)        //왼쪽 제스처 = 온수 변경
+                    state = HOT;
+                else if(adc_result[2] <= 980)   //오른쪽 제스처 = 냉수 변경
+                    state = COLD;               
+            break;
+        
+            case HOT:                           //온수 변경
+                cnt = 0;                        //대기모드 전환 카운트 초기화
+                timer2 = 0;                     //타이머2 종료를 위한 변수값 초기화
+                OCR2 = (int)21;                 //OCR2 값 변경 = 온수 방향
+                TCCR2 |= 1<<CS22 | 1<<CS20;     //타이머2 작동, 1024분주
+                TIMSK |= 1<<TOIE2;              //타이머2 인터럽트 활성화
+                state = WATER;                  //급수 모드로 복귀
+            break;
+        
+            case COLD:                          //냉수 변경
+                cnt = 0;                        //대기모드 전환 카운트 초기화
+                timer2 = 0;                     //타이머2 종료를 위한 변수값 초기화
+                OCR2 = (int)34;                 //OCR2 값 변경 = 냉수 방향
+                TCCR2 |= 1<<CS22 | 1<<CS20;     //타이머2 작동, 1024분주
+                TIMSK |= 1<<TOIE2;              //타이머2 인터럽트 활성화
+                state = WATER;                  //급수 모드로 복귀
+            break;
+        }   //end switch
+
+		adc_rq = 1;         //ADC 인터럽트 요청
+		ADCSRA |= 1<<ADSC;	// A/D 변환 시작
+		while(adc_rq);		// ADC 인터럽트 응답을 기다림
+		
+
+//##################################### 컴파일용 #####################################
+		sprintf(lcd_string[0], "%-4d", adc_result[0]);  //PIR 센서값 배열에 저장
+		LCD_str_write(0, 5, lcd_string[0]);             //PIR 센서값 LCD에 출력
+
+        sprintf(lcd_string[3], "%-4d", cnt);         //컴파일 결과 확인용 배열에 저장
+		LCD_str_write(0, 13, lcd_string[3]);             //컴파일 결과 확인용 LCD에 출력
+
+        sprintf(lcd_string[1], "%-4d", adc_result[1]);  //왼쪽 적외선 센서값 배열에 저장
+		LCD_str_write(1, 5, lcd_string[1]);              //왼쪽 적외선 센서값 LCD에 출력
+
+        sprintf(lcd_string[2], "%-4d", adc_result[2]);  //오른쪽 적외선 센서값 배열에 저장
+		LCD_str_write(1, 13, lcd_string[2]);             //오른쪽 적외선 센서값 LCD에 출력
+//########################################################################################
+    }   //end while
+    return 0;
+}
